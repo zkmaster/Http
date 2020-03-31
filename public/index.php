4090db063dd562d7de82a8b6fcafbb7cdab18eed
zhangkuan <zhangkuanlink@gmail.com>
2020-03-31 15:18:06 +0800

    add

--- public/index.php
+++ public/index.php
@@ -1,7 +1,19 @@
 <?php
 
 
-echo "hello Https ! \n";
+echo "hello Https ! <br/>";
 
 
-echo "<t1>标题一</t1>";
+echo "<h1>标题一</h1>";
+echo "<h2>标题二</h2>";
+
+// 全局配置文件
+
+// 自动加载
+
+// 初始化一些服务
+
+// 路由分发
+
+// 输出
+
